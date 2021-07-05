6e9c2b37e2328510720db85e9eb260c8ee716d90
ankangane <amolkangane101@gmail.com>
2021-07-05 18:18:14 +0530

    Amol

---
+++ EMPID46094359/ConsoleApp(Assignment)/ConsoleApp(Assignment)/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Staff
+    {
+        public int StaffID;
+        public Staff(string name, int id)
+        {
+            StaffName = name;
+            StaffID = id;
+        }
+        public string StaffName { set; get; }
+    }
+}
